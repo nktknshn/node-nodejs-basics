97f5c64a1215c5001603e22f5eb83131fbfa75e9
Nikita Kanashin <nikita@kanash.in>
2023-06-17 15:20:33 +0700

    feat: solve cli subtasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+import { parseArgs as nodeParseArgs } from "node:util";
+
+/*
+args.js - implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to the console in the format propName is value, `prop2Name is value2`
+*/
 const parseArgs = () => {
-    // Write your code here 
+    const { values } = nodeParseArgs({
+        options: {
+            propName: {
+                type: "string"
+            },
+            prop2Name: {
+                type: "string"
+            }
+        }
+    })
+
+    console.log(`propName is ${values.propName}, prop2Name is ${values.prop2Name}`);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+/* 
+env.js - implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+*/
 const parseEnv = () => {
-    // Write your code here 
+    const resultArray = []
+
+    for(const [k, v] of Object.entries(process.env)) {
+        if(k.startsWith('RSS_')) {
+            resultArray.push(`${k}=${v}`)
+        }
+    }
+
+    console.log(resultArray.join('; '));
 };
 
 parseEnv();
