ecaf4595d515924c553e59c95c3a3ac7087bfc3f
Nikita Kanashin <nikita@kanash.in>
2023-06-18 16:04:52 +0700

    feat: solve streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "fs";
+
+const readToStdout = (file) => new Promise((resolve, reject) => {
+    createReadStream(file)
+    .on('error', reject)
+    .pipe(process.stdout)
+    .on('end', () => resolve())
+})
+
+/* 
+read.js - implement function that reads file fileToRead.txt content using Readable Stream and prints it's content into process.stdout
+*/
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    // createReadStream().pipe(process.stdout)
+    await readToStdout('./src/streams/files/fileToRead.txt'); 
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from "fs";
+
+const writeFromStdin = (file) => new Promise((resolve, reject) => {
+    process.stdin
+    .pipe(createWriteStream(file))
+    .on('error', reject)
+    .on('end', () => resolve())
+})
+
+
+/* 
+write.js - implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+*/
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    await writeFromStdin('./src/streams/files/fileToWrite.txt') 
 };
 
 await write();
