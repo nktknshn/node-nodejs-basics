bee7e271ec533fd8e3a1ca401552761b9c1a09b4
Nikita Kanashin <nikita@kanash.in>
2023-06-18 12:25:48 +0700

    fix: cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,21 +1,36 @@
-import { parseArgs as nodeParseArgs } from "node:util";
-
 /*
 args.js - implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to the console in the format propName is value, `prop2Name is value2`
 */
 const parseArgs = () => {
-    const { values } = nodeParseArgs({
-        options: {
-            propName: {
-                type: "string"
-            },
-            prop2Name: {
-                type: "string"
-            }
-        }
-    })
 
-    console.log(`propName is ${values.propName}, prop2Name is ${values.prop2Name}`);
+    const args = process.argv.slice(2);
+
+    if(args.length < 2) {
+        return
+    }
+
+    const result = {}
+
+    // properties without values and values without properties will be skipped
+    // only pairs `--property-name value` are included 
+    for(let i = 0; i < args.length - 1; i++) {
+        const k = args[i]
+        const v = args[i + 1]
+
+        if(k.startsWith('--')) {
+            if(v.startsWith('--')) {
+                continue
+            }
+            result[k.slice(2)] = v
+        }
+    }
+
+    const resultString = Object.entries(result)
+                .map(([k, v]) => `${k} is ${v}`)
+                .join(', ')
+
+    console.log(resultString)
 };
 
 parseArgs();
+
