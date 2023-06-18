5164647339e60b37f35bf31b4ac3922b8507b9a3
Nikita Kanashin <nikita@kanash.in>
2023-06-18 16:03:53 +0700

    feat: solve calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createHash } from 'node:crypto'
+import { createReadStream } from 'node:fs';
+
+const getFileHash = (file) => new Promise((resolve, reject) => {
+        const hash = createHash('sha256')
+        const fileStream = createReadStream(file).on('error', reject)
+
+        fileStream.on('readable', () => {
+            while(true) {
+                const chunk = fileStream.read()
+                if(chunk === null) {
+                    return
+                }
+                hash.update(chunk)
+            }
+        })
+        .on('end', () => {
+            resolve(hash.digest('hex'))
+        })
+    })
+
+/* 
+calcHash.js - implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into console as hex
+*/
 const calculateHash = async () => {
-    // Write your code here 
+    console.log(await getFileHash('src/hash/files/fileToCalculateHashFor.txt'));
 };
 
 await calculateHash();
