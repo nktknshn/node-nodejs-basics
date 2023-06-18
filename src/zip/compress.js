e2bbf188f5567b78dff2e7f1ff89dfec51ba6e24
Nikita Kanashin <nikita@kanash.in>
2023-06-18 16:05:28 +0700

    feat: solve zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib'
+
+const compressFile = (file, outputFile) => new Promise((resolve, reject) => {
+    createReadStream(file)
+    .on('error', reject)
+    .pipe(createGzip())
+    .pipe(
+        createWriteStream(outputFile)
+        .on('error', reject)
+    )
+    .on('end', () => resolve())
+})
+
+/* 
+compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+*/
 const compress = async () => {
-    // Write your code here 
+    await compressFile(
+        'src/zip/files/fileToCompress.txt', 
+        'src/zip/files/archive.gz'
+    ) 
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+
+const decompressFile = (file, outputFile) => new Promise((resolve, reject) => {
+    createReadStream(file)
+    .on('error', reject)
+    .pipe(createGunzip().on('error', reject))
+    .pipe(
+        createWriteStream(outputFile)
+        .on('error', reject)
+    )
+    .on('end', () => resolve())
+})
+
+
+/* 
+decompress.js - implement function that decompresses archive.gz back to the fileToCompress.txt with same content as before compression using zlib and Streams API
+*/
 const decompress = async () => {
-    // Write your code here 
+    await decompressFile(
+        'src/zip/files/archive.gz',
+        'src/zip/files/fileToCompress.txt', 
+    ) 
 };
 
 await decompress();
