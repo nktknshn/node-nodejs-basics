53a6f23574dcb4024e6ecfab6496339532fe1120
Nikita Kanashin <nikita@kanash.in>
2023-06-19 16:35:45 +0700

    style: format for better readability

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -10,7 +10,9 @@ const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2
 
 const sendResult = () => {
     try {
-        parentPort.postMessage({ status: 'resolved', data: nthFibonacci(workerData.value) })
+        parentPort.postMessage({
+            status: 'resolved', data: nthFibonacci(workerData.value)
+        })
     }
     catch (e) {
         parentPort.postMessage({ status: 'error', data: null })
