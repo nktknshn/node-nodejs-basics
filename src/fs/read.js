d5f87a2f1786a4357dedc1c7aaa2cceb12c0b15d
Nikita Kanashin <nikita@kanash.in>
2023-06-19 16:31:49 +0700

    refactor: use fs module for reading instead of stream

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,17 +1,23 @@
-import { createReadStream } from 'fs';
-import { pipeline } from 'stream/promises';
-import { getAbsolutePath } from './helpers.js';
+import { promises as fs } from 'fs';
+import { exists, getAbsolutePath } from './helpers.js'
 
 /* 
 read.js - implement function that prints content of the fileToRead.txt into console (if there's no file fileToRead.txt Error with message FS operation failed must be thrown)
 */
 const read = async () => {
     const fileToRead = getAbsolutePath('./files/fileToRead.txt')
-    
-    await pipeline(
-        createReadStream(fileToRead),
-        process.stdout
-    )
+
+    if (!(await exists(fileToRead))) {
+        throw new Error('FS operation failed')
+    }
+
+    const fh = await fs.open(fileToRead, 'r')
+
+    const content = await fh.readFile({ encoding: 'utf-8' })
+
+    console.log(content)
+
+    await fh.close()
 };
 
 await read();
